8015d0d8d790aff7c170bb0cd653bdb3ff19e266
oren0e <countx@gmail.com>
2021-10-30 11:42:19 +0300

    Add better error messages

--- src/match_logic.rs
+++ src/match_logic.rs
@@ -6,11 +6,11 @@ pub fn parse_url_from_git(s: String) -> AnyhowResult<String> {
     let re = Regex::new(r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@:/\-~]+)(\.git)(/)?")?;
     let url_parts = re
         .captures(&s)
-        .ok_or_else(|| anyhow!("Regex failed to capture groups"))?;
+        .ok_or_else(|| anyhow!("Git repository not found"))?;
     let domain_re = Regex::new(r".*@(.*)$")?;
     let match_domain = domain_re
         .captures(&url_parts[1])
-        .ok_or_else(|| anyhow!("Regex failed to capture groups"))?;
+        .ok_or_else(|| anyhow!("Regex error capturing domain"))?;
 
     let result: String =
         "https://".to_string() + &match_domain[1].to_string() + r"/" + &url_parts[7].to_string();
