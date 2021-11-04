4f7b5f38173853fe7dd59745644096146f7cef47
oren0e <33298364+oren0e@users.noreply.github.com>
2021-11-04 21:25:14 +0200

    Add push and open PR in browser feature (#2)
    * Add clap to cargo.toml and change version to 1.1.0 in cargo.toml
    
    * Move action functions to separate module and add the push and pr function
    
    * Remove actions from main and add clap logic
    
    * Strip new line character from current branch
    
    * Use stderr instead of stdout to capture response from git server
    
    * Add printing version info to CI
    
    * Use dtolnay github action for fixing rust version and using components
    
    * Change test of push server output parsing
    
    * Update readme
    
    * Take version automatically from cargo toml file

---
+++ src/actions.rs
@@ -0,0 +1,74 @@
+use crate::match_logic::parse_url_from_git;
+use anyhow::anyhow;
+use anyhow::Result as AnyhowResult;
+use regex::Regex;
+use std::process::{Command, Stdio};
+
+pub fn open_repo() -> AnyhowResult<()> {
+    let git_repo = Command::new("git")
+        .args(&["config", "--get", "remote.origin.url"])
+        .stdout(Stdio::piped())
+        .output()?;
+
+    let stdout = String::from_utf8(git_repo.stdout)?;
+    let parsed_url = parse_url_from_git(stdout)?;
+    webbrowser::open(&parsed_url)?;
+    Ok(())
+}
+
+pub fn push_and_open_pr() -> AnyhowResult<()> {
+    let current_branch = Command::new("git")
+        .args(&["branch", "--show-current"])
+        .stdout(Stdio::piped())
+        .output()?;
+    let current_branch_text = &String::from_utf8(current_branch.stdout)?;
+    let current_branch_text_stripped = current_branch_text.trim();
+    let output_from_push = Command::new("git")
+        .args(&["push", "origin", &current_branch_text_stripped])
+        .stderr(Stdio::piped())
+        .output()?;
+    let pr_re = Regex::new(&format!(
+        r"remote:.*(https.*{}).*\n",
+        current_branch_text_stripped
+    ))?;
+    let output_from_push_text = String::from_utf8(output_from_push.stderr)?;
+    println!("{:?}", &output_from_push_text);
+    let captured = pr_re
+        .captures(&output_from_push_text)
+        .ok_or_else(|| anyhow!("Error capturing PR url"))?;
+    println!("{:?}", &captured[1]);
+    webbrowser::open(&captured[1])?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_correct_pr_parsing_from_output() {
+        let output = r#"Counting objects: 4, done.
+Delta compression using up to 12 threads.
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (4/4), 3.01 KiB | 3.01 MiB/s, done.
+Total 4 (delta 2), reused 0 (delta 0)
+remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+remote:
+remote: Create a pull request for 'feat/add-more-pokemons' on GitHub by visiting:
+remote:      https://github.com/tobiasbueschel/awesome-pokemon/pull/new/feat/add-more-pokemons
+remote:
+To github.com:tobiasbueschel/awesome-pokemon.git
+ * [new branch]      feat/add-more-pokemons -> feat/add-more-pokemons"#;
+        let current_branch = "feat/add-more-pokemons\n";
+        let current_branch_text_stripped = current_branch.trim();
+        let re = Regex::new(&format!(
+            r"remote:.*(https.*{}).*\n",
+            current_branch_text_stripped
+        ))
+        .unwrap();
+        let captured = re.captures(&output).unwrap();
+        println!("{:?}", &captured[1]);
+        assert!(&captured[1].starts_with("https"));
+        assert!(&captured[1].ends_with("add-more-pokemons"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use crate::match_logic::parse_url_from_git;
-use anyhow::Result as AnyhowResult;
-use std::process::{Command, Stdio};
+extern crate clap;
+use clap::{crate_version, App, Arg};
 
+use crate::actions::{open_repo, push_and_open_pr};
+use anyhow::Result as AnyhowResult;
+
+mod actions;
 mod match_logic;
 
 fn main() -> AnyhowResult<()> {
-    let git_repo = Command::new("git")
-        .args(&["config", "--get", "remote.origin.url"])
-        .stdout(Stdio::piped())
-        .output()?;
-
-    let stdout = String::from_utf8(git_repo.stdout)?;
-    let parsed_url = parse_url_from_git(stdout)?;
-    webbrowser::open(&parsed_url)?;
-    Ok(())
+    let matches = App::new("Gitopen")
+        .version(crate_version!())
+        .author("Oren Epshtain")
+        .about("Utility to open repo from terminal and pull requests after push")
+        .arg(
+            Arg::with_name("push_and_pr")
+                .short("p")
+                .long("push-open-pr")
+                .help("Pushes to current branch and opens corresponding PR"),
+        )
+        .get_matches();
+    if matches.is_present("push_and_pr") {
+        push_and_open_pr()?;
+        Ok(())
+    } else {
+        open_repo()?;
+        Ok(())
+    }
 }
